c4a9d721e05678c01be3a9a8a59fe6177182d109
adriano <adr.amm2017@gmail.com>
2018-07-09 23:12:35 -0300

    FIX

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
-
 <!DOCTYPE html>
 <html>
 <head>
    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Atividade Reflexiva">
-    <meta name="author" content="Adriano Mello Marcelino">
+    <meta name="author" content="Adriano">
     <title>Gerador de Currículo</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-
 </head>
-<body>
 
-     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Gerador de Currículos</a>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><h3>Gerador de Currículos</h3></a>
     </nav>
+
     <main class="container" style="margin-top:20px">
         <form action="gerarCurriculo.php" method="post" target="_blank" enctype="multipart/form-data">
             <div class="card">
@@ -27,6 +25,7 @@
                         <label for="nome">Nome</label>
                         <input type="text" class="form-control" name="nome" id="nome">
                     </div>
+                    
                     <div class="row">
                         <div class="col">
                             <div class="form-group">
@@ -34,6 +33,7 @@
                                 <input type="text" class="form-control" name="cargo" id="cargo">
                             </div>
                         </div>
+                        
                         <div class="col">
                             <div class="form-group">
                                 <label for="foto">Foto</label>
@@ -41,10 +41,12 @@
                             </div>
                         </div>
                     </div>
+                    
                     <div class="form-group">
                         <label for="endereco">Endereço</label>
                         <input type="text" class="form-control" name="endereco" id="endereco">
                     </div>
+                    
                     <div class="row">
                         <div class="col">
                             <div class="form-group">
@@ -52,6 +54,7 @@
                                 <input type="text" class="form-control" name="telefone" id="telefone">
                             </div>
                         </div> 
+                        
                         <div class="col">
                             <div class="form-group">
                                 <label for="email">E-mail</label>
@@ -59,11 +62,13 @@
                             </div>
                         </div> 
                     </div>
+                    
                     <div class="form-group">
                         <label for="resumo">Resumo</label>
                         <textarea class="form-control" name="resumo" id="resumo"></textarea>
                     </div>
                 </div>
+                
                 <div class="card-body" id="div-formacoes">
                     <h4 class="card-title">Formação</h4>
                     <button class="btn btn-sm right" id="btn-adicionar-formacao" title="Adicionar formação">Adicionar formação</button>
@@ -76,10 +81,9 @@
 
                 <div class="card-body" id="div-experiencias">
                     
-                    </div>
+                    
                 </div>
 
-                
                 <div class="card-footer">
                     <button class="btn btn-primary" type="submit">Gerar currículo</button>
                     
--- modelos/modelo1.php
+++ modelos/modelo1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Currículo | Modelo 1</title>
+    <title>Currículo </title>
     <link rel="stylesheet" href="css/modelo1.css">
 </head>
 <body>
